4bbccd2cec23f0e91b9e28608a06d8d04df12abe
Hidetatz Yaginuma in CI <hidetatz@gmail.com>
2025-04-13 08:41:59 +0900

    support fullwidth char

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 turtle editor
+
+## keymaps
+
+By default, turtle editor is in normal mode.
+
+### normal mode
+
+* `i`: enter insert mode
+* `h`: move left
+* `j`: move down
+* `k`: move up
+* `l`: move right
+
+### insert mode
+
+In insert mode, you can edit the text.
+
+* `Esc`: exit insert mode
--- main.go
+++ main.go
@@ -19,6 +19,227 @@ const (
 	insert
 )
 
+type line struct {
+	buffer []rune
+}
+
+func (l *line) String() string {
+	return string(l.buffer)
+}
+
+func (l *line) width() int {
+	return len(l.buffer)
+}
+
+func (l *line) copy() *line {
+	return &line{buffer: slices.Clone(l.buffer)}
+}
+
+func likelyFullwidth(r rune) bool {
+	fullwidth := &unicode.RangeTable{
+		R16: []unicode.Range16{
+			{0x3000, 0x3000, 1},
+			{0xff01, 0xff60, 1},
+			{0xffe0, 0xffe6, 1},
+		},
+	}
+
+	wide := &unicode.RangeTable{
+		R16: []unicode.Range16{
+			{0x1100, 0x115F, 1},
+			{0x2329, 0x232A, 1},
+			{0x2E80, 0x2FFB, 1},
+			{0x3001, 0x303E, 1},
+			{0x3041, 0x33FF, 1},
+			{0x3400, 0x4DB5, 1},
+			{0x4E00, 0x9FBB, 1},
+			{0xA000, 0xA4C6, 1},
+			{0xAC00, 0xD7A3, 1},
+			{0xF900, 0xFAD9, 1},
+			{0xFE10, 0xFE19, 1},
+			{0xFE30, 0xFE6B, 1},
+		},
+		R32: []unicode.Range32{
+			{0x20000, 0x2A6D6, 1},
+			{0x2A6D7, 0x2F7FF, 1},
+			{0x2F800, 0x2FA1D, 1},
+			{0x2FA1E, 0x2FFFD, 1},
+			{0x30000, 0x3FFFD, 1},
+		},
+	}
+
+	ambiguous := &unicode.RangeTable{
+		R16: []unicode.Range16{
+			{0x00A1, 0x00A1, 1},
+			{0x00A4, 0x00A4, 1},
+			{0x00A7, 0x00A8, 1},
+			{0x00AA, 0x00AA, 1},
+			{0x00AD, 0x00AE, 1},
+			{0x00B0, 0x00B4, 1},
+			{0x00B6, 0x00BA, 1},
+			{0x00BC, 0x00BF, 1},
+			{0x00C6, 0x00C6, 1},
+			{0x00D0, 0x00D0, 1},
+			{0x00D7, 0x00D8, 1},
+			{0x00DE, 0x00E1, 1},
+			{0x00E6, 0x00E6, 1},
+			{0x00E8, 0x00EA, 1},
+			{0x00EC, 0x00ED, 1},
+			{0x00F0, 0x00F0, 1},
+			{0x00F2, 0x00F3, 1},
+			{0x00F7, 0x00FA, 1},
+			{0x00FC, 0x00FC, 1},
+			{0x00FE, 0x00FE, 1},
+			{0x0101, 0x0101, 1},
+			{0x0111, 0x0111, 1},
+			{0x0113, 0x0113, 1},
+			{0x011B, 0x011B, 1},
+			{0x0126, 0x0127, 1},
+			{0x012B, 0x012B, 1},
+			{0x0131, 0x0133, 1},
+			{0x0138, 0x0138, 1},
+			{0x013F, 0x0142, 1},
+			{0x0144, 0x0144, 1},
+			{0x0148, 0x014B, 1},
+			{0x014D, 0x014D, 1},
+			{0x0152, 0x0153, 1},
+			{0x0166, 0x0167, 1},
+			{0x016B, 0x016B, 1},
+			{0x01CE, 0x01CE, 1},
+			{0x01D0, 0x01D0, 1},
+			{0x01D2, 0x01D2, 1},
+			{0x01D4, 0x01D4, 1},
+			{0x01D6, 0x01D6, 1},
+			{0x01D8, 0x01D8, 1},
+			{0x01DA, 0x01DA, 1},
+			{0x01DC, 0x01DC, 1},
+			{0x0251, 0x0251, 1},
+			{0x0261, 0x0261, 1},
+			{0x02C4, 0x02C4, 1},
+			{0x02C7, 0x02C7, 1},
+			{0x02C9, 0x02CB, 1},
+			{0x02CD, 0x02CD, 1},
+			{0x02D0, 0x02D0, 1},
+			{0x02D8, 0x02DB, 1},
+			{0x02DD, 0x02DD, 1},
+			{0x02DF, 0x02DF, 1},
+			{0x0300, 0x036F, 1},
+			{0x0391, 0x03A9, 1},
+			{0x03B1, 0x03C1, 1},
+			{0x03C3, 0x03C9, 1},
+			{0x0401, 0x0401, 1},
+			{0x0410, 0x044F, 1},
+			{0x0451, 0x0451, 1},
+			{0x2010, 0x2010, 1},
+			{0x2013, 0x2016, 1},
+			{0x2018, 0x2019, 1},
+			{0x201C, 0x201D, 1},
+			{0x2020, 0x2022, 1},
+			{0x2024, 0x2027, 1},
+			{0x2030, 0x2030, 1},
+			{0x2032, 0x2033, 1},
+			{0x2035, 0x2035, 1},
+			{0x203B, 0x203B, 1},
+			{0x203E, 0x203E, 1},
+			{0x2074, 0x2074, 1},
+			{0x207F, 0x207F, 1},
+			{0x2081, 0x2084, 1},
+			{0x20AC, 0x20AC, 1},
+			{0x2103, 0x2103, 1},
+			{0x2105, 0x2105, 1},
+			{0x2109, 0x2109, 1},
+			{0x2113, 0x2113, 1},
+			{0x2116, 0x2116, 1},
+			{0x2121, 0x2122, 1},
+			{0x2126, 0x2126, 1},
+			{0x212B, 0x212B, 1},
+			{0x2153, 0x2154, 1},
+			{0x215B, 0x215E, 1},
+			{0x2160, 0x216B, 1},
+			{0x2170, 0x2179, 1},
+			{0x2190, 0x2199, 1},
+			{0x21B8, 0x21B9, 1},
+			{0x21D2, 0x21D2, 1},
+			{0x21D4, 0x21D4, 1},
+			{0x21E7, 0x21E7, 1},
+			{0x2200, 0x2200, 1},
+			{0x2202, 0x2203, 1},
+			{0x2207, 0x2208, 1},
+			{0x220B, 0x220B, 1},
+			{0x220F, 0x220F, 1},
+			{0x2211, 0x2211, 1},
+			{0x2215, 0x2215, 1},
+			{0x221A, 0x221A, 1},
+			{0x221D, 0x2220, 1},
+			{0x2223, 0x2223, 1},
+			{0x2225, 0x2225, 1},
+			{0x2227, 0x222C, 1},
+			{0x222E, 0x222E, 1},
+			{0x2234, 0x2237, 1},
+			{0x223C, 0x223D, 1},
+			{0x2248, 0x2248, 1},
+			{0x224C, 0x224C, 1},
+			{0x2252, 0x2252, 1},
+			{0x2260, 0x2261, 1},
+			{0x2264, 0x2267, 1},
+			{0x226A, 0x226B, 1},
+			{0x226E, 0x226F, 1},
+			{0x2282, 0x2283, 1},
+			{0x2286, 0x2287, 1},
+			{0x2295, 0x2295, 1},
+			{0x2299, 0x2299, 1},
+			{0x22A5, 0x22A5, 1},
+			{0x22BF, 0x22BF, 1},
+			{0x2312, 0x2312, 1},
+			{0x2460, 0x24E9, 1},
+			{0x24EB, 0x254B, 1},
+			{0x2550, 0x2573, 1},
+			{0x2580, 0x258F, 1},
+			{0x2592, 0x2595, 1},
+			{0x25A0, 0x25A1, 1},
+			{0x25A3, 0x25A9, 1},
+			{0x25B2, 0x25B3, 1},
+			{0x25B6, 0x25B7, 1},
+			{0x25BC, 0x25BD, 1},
+			{0x25C0, 0x25C1, 1},
+			{0x25C6, 0x25C8, 1},
+			{0x25CB, 0x25CB, 1},
+			{0x25CE, 0x25D1, 1},
+			{0x25E2, 0x25E5, 1},
+			{0x25EF, 0x25EF, 1},
+			{0x2605, 0x2606, 1},
+			{0x2609, 0x2609, 1},
+			{0x260E, 0x260F, 1},
+			{0x2614, 0x2615, 1},
+			{0x261C, 0x261C, 1},
+			{0x261E, 0x261E, 1},
+			{0x2640, 0x2640, 1},
+			{0x2642, 0x2642, 1},
+			{0x2660, 0x2661, 1},
+			{0x2663, 0x2665, 1},
+			{0x2667, 0x266A, 1},
+			{0x266C, 0x266D, 1},
+			{0x266F, 0x266F, 1},
+			{0x273D, 0x273D, 1},
+			{0x2776, 0x277F, 1},
+			{0xE000, 0xF8FF, 1},
+			{0xFE00, 0xFE0F, 1},
+			{0xFFFD, 0xFFFD, 1},
+		},
+		R32: []unicode.Range32{
+			{0xE0100, 0xE01EF, 1},
+			{0xF0000, 0xFFFFD, 1},
+			{0x100000, 0x10FFFD, 1},
+		},
+	}
+
+	return unicode.Is(fullwidth, r) || unicode.Is(wide, r) || unicode.Is(ambiguous, r)
+}
+
+func (l *line) insert(r rune, at int) {
+	l.buffer = slices.Insert(l.buffer, at, r)
+}
+
 type cursorpos struct {
 	x, y int // left, top is (0, 0)
 }
@@ -28,7 +249,7 @@ type screen struct {
 	cols   int
 	mode   mode
 	cursor cursorpos
-	lines  [][]rune
+	lines  []*line
 }
 
 type direction int
@@ -41,7 +262,16 @@ const (
 )
 
 func (s *screen) rendercursor() {
-	movecursor(s.cursor.x, s.cursor.y)
+	line := s.lines[s.cursor.y]
+	x := 0
+	for i := range s.cursor.x {
+		width := 1
+		if likelyFullwidth(line.buffer[i]) {
+			width = 2
+		}
+		x += width
+	}
+	movecursor(x, s.cursor.y)
 }
 
 func (s *screen) moveCursor(direction direction) bool {
@@ -57,8 +287,8 @@ func (s *screen) moveCursor(direction direction) bool {
 
 		// if upper line is shorter, the x should be the end of the upper line
 		upperline := s.lines[s.cursor.y]
-		if len(upperline) < current.x {
-			s.cursor.x = len(upperline)
+		if upperline.width() < current.x {
+			s.cursor.x = upperline.width()
 		}
 
 		return true
@@ -72,8 +302,8 @@ func (s *screen) moveCursor(direction direction) bool {
 
 		// if next line is shorter, the x should be the end of the next line
 		nextline := s.lines[s.cursor.y]
-		if len(nextline) < current.x {
-			s.cursor.x = len(nextline)
+		if nextline.width() < current.x {
+			s.cursor.x = nextline.width()
 		}
 
 		return true
@@ -88,7 +318,7 @@ func (s *screen) moveCursor(direction direction) bool {
 		return true
 
 	case right:
-		if current.x == len(s.lines[current.y]) {
+		if current.x == s.lines[current.y].width() {
 			return false
 		}
 
@@ -147,8 +377,8 @@ func main() {
 	s := &screen{
 		rows:  row,
 		cols:  col,
-		lines: [][]rune{{}}, // initialize first line
-		mode:  normal,       // normal mode on startup
+		lines: []*line{{}}, // initialize first line
+		mode:  normal,      // normal mode on startup
 	}
 
 	refresh()
@@ -231,8 +461,8 @@ func main() {
 
 			default:
 				// edit the line on memory
-				line := slices.Clone(s.lines[s.cursor.y])
-				line = slices.Insert(line, s.cursor.x, r)
+				line := s.lines[s.cursor.y].copy()
+				line.insert(r, s.cursor.x)
 				s.lines[s.cursor.y] = line
 
 				debug("%v, %v, %v\n", s.lines, s.cursor.x, s.cursor.y)
@@ -244,7 +474,7 @@ func main() {
 				// render
 				clearline()
 				resetcursor()
-				fmt.Fprint(os.Stdout, fmt.Sprintf("%s", string(line)))
+				fmt.Fprint(os.Stdout, fmt.Sprintf("%s", line.String()))
 
 				// restore cursor position
 				s.cursor.x = curcursor.x
